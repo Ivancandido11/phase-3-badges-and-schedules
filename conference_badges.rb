72b91e3fc8acb717cac6efde197bc653df597f03
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-04 15:28:28 -0400

    Completed Assingment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+  badges.map do |badge|
+    badge_maker(badge)
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index do |person, index|
+    "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(attendees)
+  attendees.map.with_index do |person, index|
+    puts badge_maker(person)
+    puts "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+  end
+end
